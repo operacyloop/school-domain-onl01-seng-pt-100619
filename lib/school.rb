c66be96209d89977af3f87500eb86d41c892f2a6
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-11-02 20:27:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School(name) #this is the model in the domain model context
+  
+  attr_accessor :roster
+  
+  ROSTER = {}
+  
+end 
+
+def add_student (name, gr)
+ROSTER << @student
+
+school = School.new("Bayside High School")
+
+#
+#
+
+class School
+  attr_accessor :roster #, :page_count
+  #attr_reader  :title, :genre
+
+ ROSTER = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+  def genre=(genre)
+    @genre = genre 
+    GENRES << genre
+  end
+
+endc
